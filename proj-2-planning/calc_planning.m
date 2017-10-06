98144d974401fba5cc96dd76679d56027c958395
Deathn0t <romainegele@gmail.com>
2017-10-06 19:51:49 +0200

    add files + calc_planning

---
+++ proj-2-planning/calc_planning.m
@@ -0,0 +1,6 @@
+% Solve the min pb about planning with linprog
+
+% Prof related number
+profNames =   {'Droite', 'Ellips', 'Proton', 'Pascal', 'Delle', 'Young', 'Gazelle', 'Bigceps'};
+profNums = [1, 2, 3, 4, 5, 6, 7, 8];
+profs = containers.Map(profNames, profNums);

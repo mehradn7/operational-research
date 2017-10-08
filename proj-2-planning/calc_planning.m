ff5080ab196834954593dcdf6392ed30a3d4b306
Deathn0t <romainegele@gmail.com>
2017-10-08 14:34:27 +0200

    affection de A pour les contraintes (II)

--- proj-2-planning/calc_planning.m
+++ proj-2-planning/calc_planning.m
@@ -31,11 +31,28 @@ profNums = 1:p;
 profs = containers.Map(profNames, profNums); % profs('Droite') return 1
 d = 5;
 t = 4;
+A = zeros(size(x,2), size(b,2));
 
 % constraints : I
 
 
-% constraints : II
+% constraints : II, David & Romain
+begin_2 = 0; % offset const to avoid conflicts of affectation with other constraints
+n = 0; % (n+1)th constraint
+for di = 1:d % loop on days, di is the current day
+  for k = 1*di:t*di % loop on slots, k is the current slot in [|1, d*t|]
+    A(begin_2+1, threeD2oneD(1,1,k)) = 1; % 1 : maths (a)
+    A(begin_2+2, threeD2oneD(2,2,k)) = 1; % 2 : maths (b)
+    for j = 1:c
+      A(begin_2+2+j, threeD2oneD(3,j,k)) = 1; % 3, 4 : physique (a)
+      A(begin_2+6+j, threeD2oneD(6,j,k)) = 1; % 7, 8 : anglais (a)
+    end
+    A(begin_2+5, threeD2oneD(4,1,k)) = 1; % 5 : informatique (a)
+    A(begin_2+6, threeD2oneD(5,2,k)) = 1; % 6 : informatique (b)
+    A(begin_2+9, threeD2oneD(7,1,k)) = 1; % 9 : sport (a)
+    A(begin_2+10, threeD2oneD(8,2,k)) = 1; % 10 : sport (b)
+  end
+end
 
 
 % constraints : III

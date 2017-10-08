156c3a21f87c8257f02a2788c84ea75bb126d66d
Deathn0t <romainegele@gmail.com>
2017-10-08 11:12:22 +0200

    Présentation du fichier calc_planning où linprog(...) sera appelée.

--- proj-2-planning/calc_planning.m
+++ proj-2-planning/calc_planning.m
@@ -1,6 +1,42 @@
 % Solve the min pb about planning with linprog
 
+% === Variables ===
+% i_x : index in vector X
+% i_m : index in matrix X, teachers (profs)
+% j_m : index in matrix X, classes (promos)
+% k_m : index in matrix X, slot (créneaux)
+% profs : dic with prof names and corresponding value
+% c : number of classes
+% p : number of teachers
+% d : number of days
+% t : number of slots/days, d*t : total number of slots in a week
+% Aeq, beq : represent egality constraints
+% A, b : represent inegality constraints
+% == === === === ==
+
+% == bijection between i_x <-> (i_m, j_m, k_m) ==
+% i_x = (i_m-1)*p*d*t + (j_m-1)*d*t + k_m
+% i_m = fix(i_x/(p*d*t)) + 1
+% rest_i_m = mod(i_m, p*d*t)
+% j_m = fix(rest_i_m/(d*t)) + 1
+% k_m = mod(rest_i_m, d*t)
+% == === === === ==
+
+% == Init parameters ==
 % Prof related number
+c = 2;
 profNames =   {'Droite', 'Ellips', 'Proton', 'Pascal', 'Delle', 'Young', 'Gazelle', 'Bigceps'};
-profNums = [1, 2, 3, 4, 5, 6, 7, 8];
-profs = containers.Map(profNames, profNums);
+p = size(profNames, 2);
+profNums = 1:p;
+profs = containers.Map(profNames, profNums); % profs('Droite') return 1
+d = 5;
+t = 4;
+
+% constraints : I
+
+
+% constraints : II
+
+
+% constraints : III
+% constraints : IV

9b89e27a962ed80ad19622161d012d288e1be5af
Deathn0t <romainegele@gmail.com>
2017-10-08 15:00:00 +0200

    affectation des contraintes (II) pour A,b par intervalles.

--- proj-2-planning/calc_planning.m
+++ proj-2-planning/calc_planning.m
@@ -31,6 +31,7 @@ profNums = 1:p;
 profs = containers.Map(profNames, profNums); % profs('Droite') return 1
 d = 5;
 t = 4;
+b = zeros(nb_constraints, 1);
 A = zeros(size(x,2), size(b,2));
 
 % constraints : I
@@ -40,16 +41,18 @@ A = zeros(size(x,2), size(b,2));
 begin_2 = 0; % offset const to avoid conflicts of affectation with other constraints
 n = 0; % (n+1)th constraint
 for di = 1:d % loop on days, di is the current day
-  K = 1*di:t*di; % loop on slots, k is the current slot in [|1, d*t|]
+  K = (di-1)*t+1:t*di; % scale on slots
   A(begin_2+1, threeD2oneD(1,1,K)) = ones(1,t); % 1 : maths (a)
   A(begin_2+2, threeD2oneD(2,2,K)) = ones(1,t); % 2 : maths (b)
-  J = 1:c;
+  J = 1:c; % scale on classes
   A(begin_2+3:begin_2+3+(c-1), threeD2oneD(3,J,K)) = ones(1,t); % 3, 4 : physique (a)
   A(begin_2+7:begin_2+7+(c-1), threeD2oneD(6,J,K)) = ones(1,t); % 7, 8 : anglais (a)
-  A(begin_2+5, threeD2oneD(4,1,K)) = 2*ones(1,t); % 5 : informatique (a)
-  A(begin_2+6, threeD2oneD(5,2,K)) = 2*ones(1,t); % 6 : informatique (b)
+  A(begin_2+5, threeD2oneD(4,1,K)) = ones(1,t); % 5 : informatique (a)
+  A(begin_2+6, threeD2oneD(5,2,K)) = ones(1,t); % 6 : informatique (b)
   A(begin_2+9, threeD2oneD(7,1,K)) = ones(1,t); % 9 : sport (a)
   A(begin_2+10, threeD2oneD(8,2,K)) = ones(1,t); % 10 : sport (b)
+  b(begin_2+1:begin_2+10) = ones(10,1); % for all constraints = 1
+  b(begin_2+5:begin_2+6) = 2*ones(2,1); % 5,6 : informatique (a,b)
 end
 
 

ce50f3f2d816f4e6f6061fc417a366b063764061
Zor0d <ddiochot@gmail.com>
2017-10-10 23:52:26 +0200

    Utilisation des variables begin_eq et begin_ineq correctement. Suppression des variables inutiles.

--- proj-1-planning/calc_planning.m
+++ proj-1-planning/calc_planning.m
@@ -34,9 +34,17 @@ t = 4;
 b = zeros(nb_constraints, 1);
 A = zeros(size(x,2), size(b,2));
 
+I = 1:p; %scale on teachers
+J = 1:c; % scale on classes
+
+
 % constraints : I
-begin_ineq = 0;
-begin_eq =0;
+
+% offset const to avoid conflicts of affectation with other constraints
+
+begin_ineq = 1; % indice de la ligne a modifié dans A et b
+begin_eq = 1; % indice de la ligne a modifié dans Aeq et beq
+
 
 for j=1:c
     for k=1:t*d
@@ -58,72 +66,71 @@ end
 
 
 % constraints : II, David & Romain
-begin_2 = 0; % offset const to avoid conflicts of affectation with other constraints
+% offset const to avoid conflicts of affectation with other constraints
 n = 0; % (n+1)th constraint
 for di = 1:d % loop on days, di is the current day
   K = (di-1)*t+1:t*di; % scale on slots
-  A(begin_2+1, threeD2oneD(1,1,K)) = ones(1,t); % 1 : maths (a)
-  A(begin_2+2, threeD2oneD(2,2,K)) = ones(1,t); % 2 : maths (b)
+  A(begin_ineq+1, threeD2oneD(1,1,K)) = ones(1,t); % 1 : maths (a)
+  A(begin_ineq+2, threeD2oneD(2,2,K)) = ones(1,t); % 2 : maths (b)
   J = 1:c; % scale on classes
 	for j=1:c
-		A(begin_2+3+(j-1), threeD2oneD(3,j,K)) = ones(1,t); % 3, 4 : physique (a)
-		A(begin_2+7+(j-1), threeD2oneD(6,j,K)) = ones(1,t); % 7, 8 : anglais (a)
+		A(begin_ineq+3+(j-1), threeD2oneD(3,j,K)) = ones(1,t); % 3, 4 : physique (a)
+		A(begin_ineq+7+(j-1), threeD2oneD(6,j,K)) = ones(1,t); % 7, 8 : anglais (a)
 	end
-  A(begin_2+5, threeD2oneD(4,1,K)) = ones(1,t); % 5 : informatique (a)
-  A(begin_2+6, threeD2oneD(5,2,K)) = ones(1,t); % 6 : informatique (b)
-  A(begin_2+9, threeD2oneD(7,1,K)) = ones(1,t); % 9 : sport (a)
-  A(begin_2+10, threeD2oneD(8,2,K)) = ones(1,t); % 10 : sport (b)
-  b(begin_2+1:begin_2+10) = ones(10,1); % for all constraints = 1
-  b(begin_2+5:begin_2+6) = 2*ones(2,1); % 5,6 : informatique (a,b)
-	begin_2 = begin_2 + 10;
+  A(begin_ineq+5, threeD2oneD(4,1,K)) = ones(1,t); % 5 : informatique (a)
+  A(begin_ineq+6, threeD2oneD(5,2,K)) = ones(1,t); % 6 : informatique (b)
+  A(begin_ineq+9, threeD2oneD(7,1,K)) = ones(1,t); % 9 : sport (a)
+  A(begin_ineq+10, threeD2oneD(8,2,K)) = ones(1,t); % 10 : sport (b)
+  b(begin_ineq+1:begin_ineq+10) = ones(10,1); % for all constraints = 1
+  b(begin_ineq+5:begin_ineq+6) = 2*ones(2,1); % 5,6 : informatique (a,b)
+  begin_ineq = begin_ineq + 10;
 end
 
 
 % constraints : III
 
-begin_3 = 0; % offset const to avoid conflicts of affectation with other constraints
-I=1:p; %scale on teachers
-J = 1:c; % scale on classes
 
 %Les cours de sport ont lieu le jeudi après-midi de 14h à 16h
-Aeq(begin_3+1,threeD2oneD(7,1,15))=1;
-beq(begin_3+1,threeD2oneD(7,1,15))=1;
-Aeq(begin_3+2,threeD2oneD(8,2,15))=1;
-beq(begin_3+1,threeD2oneD(8,2,15))=1;
-Aeq(begin_3+10,:)=ones(1,size(x,1));
-Aeq(begin_3+10,threeD2oneD(7,1,15))=0;
-Aeq(begin_3+11,:)=ones(1,size(x,1));
-Aeq(begin_3+11,threeD2oneD(8,2,15))=0;
+Aeq(begin_eq+1,threeD2oneD(7,1,15))=1;
+beq(begin_eq+1,threeD2oneD(7,1,15))=1;
+Aeq(begin_eq+2,threeD2oneD(8,2,15))=1;
+beq(begin_eq+1,threeD2oneD(8,2,15))=1;
+Aeq(begin_eq+10,:)=ones(1,size(x,1));
+Aeq(begin_eq+10,threeD2oneD(7,1,15))=0;
+Aeq(begin_eq+11,:)=ones(1,size(x,1));
+Aeq(begin_eq+11,threeD2oneD(8,2,15))=0;
 
 %Mr Young(6), 3 cours pour la promo 1 et 3 cours pour la promo 2
-Aeq(begin_3+3,threeD2oneD(6,1,1:d*t))=ones(1,d*t);
-beq(begin_3+3)=3;
-Aeq(begin_3+4,threeD2oneD(6,2,1:d*t))=ones(1,d*t);
-beq(begin_3+4)=3;
+Aeq(begin_eq+3,threeD2oneD(6,1,1:d*t))=ones(1,d*t);
+beq(begin_eq+3)=3;
+Aeq(begin_eq+4,threeD2oneD(6,2,1:d*t))=ones(1,d*t);
+beq(begin_eq+4)=3;
 %Le premier créneau du lundi matin est réservé au partiel
-Aeq(begin_3+5,threeD2oneD(I,1,1))=ones(1,p);
-Aeq(begin_3+6,threeD2oneD(I,2,1))=ones(1,p);
-beq(begin_3+5)=0;
-beq(begin_3+6)=0;
+Aeq(begin_eq+5,threeD2oneD(I,1,1))=ones(1,p);
+Aeq(begin_eq+6,threeD2oneD(I,2,1))=ones(1,p);
+beq(begin_eq+5)=0;
+beq(begin_eq+6)=0;
 %Mr Ellips(2) est indisponible le lundi matin
-Aeq(begin_3+7,threeD2oneD(2,2,1:2))=ones(1,2);
-beq(begin_3+7)=0;
+Aeq(begin_eq+7,threeD2oneD(2,2,1:2))=ones(1,2);
+beq(begin_eq+7)=0;
 %Mme Proton (3) ne peut pas travailler le mercredi
 mercredi=9:12;
-Aeq(begin_3+8,threeD2oneD(3,1,mercredi))=ones(1,t);
-Aeq(begin_3+9,threeD2oneD(3,2,mercredi))=ones(1,t);
-beq(begin_3+8)=0;
-beq(begin_3+9)=0;
+Aeq(begin_eq+8,threeD2oneD(3,1,mercredi))=ones(1,t);
+Aeq(begin_eq+9,threeD2oneD(3,2,mercredi))=ones(1,t);
+beq(begin_eq+8)=0;
+beq(begin_eq+9)=0;
+
+begin_eq = begin_eq + 12;
 
 %Un prof a un cours avec une seule promo dans un creneau Proton (3) et
 %Young (6)
-%i_ineq: indice courant des inégalités
+%begin_ineq: indice courant des inégalités
 for k = 1:d*t % loop on slots, k is the current slot
-    A(i_ineq+1,threeD2oneD(3,J,k))=ones(1,2);
-    b(i_ineq+1)=1;
-    A(i_ineq+2,threeD2oneD(6,J,k))=ones(1,2);
-    b(i_ineq+2)=1;
-    i_ineq=i_ineq+2;
+    A(begin_ineq+1,threeD2oneD(3,J,k))=ones(1,2);
+    b(begin_ineq+1)=1;
+    A(begin_ineq+2,threeD2oneD(6,J,k))=ones(1,2);
+    b(begin_ineq+2)=1;
+    begin_ineq=begin_ineq+2;
 end
 % constraints : IV
 
@@ -194,7 +201,6 @@ begin_eq=begin_eq+1;
 
 % Plusieurs professeurs ne peuvent pas donner cours sur un même créneau
 
-I=1:m;
 for j=1:c
     for k=1:d*t
       A(begin_ineq, threeD2oneD(I,j,k))=ones(1,m);
@@ -205,7 +211,6 @@ end
 
 % Le professeur Young ne peut pas donner cours à plusieurs promos sur le même créneau
 
-J=1:c;
 for k=1:d*t
     A(begin_ineq, threeD2oneD(6,J,k))=ones(1,c);
     b(begin_ineq)=1;
@@ -214,7 +219,6 @@ end
 
 % Le professeur Proton ne peut pas donner cours à plusieurs promos sur le même créneau
 
-J=1:c;
 for k=1:d*t
     A(begin_ineq, threeD2oneD(3,J,k))=ones(1,c);
     b(begin_ineq)=1;

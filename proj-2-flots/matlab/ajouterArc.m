be4882314a55bcdb0530a09af53d500427ebcb42
Deathn0t <romainegele@gmail.com>
2017-10-22 18:44:04 +0200

    started ajouterArc

---
+++ proj-2-flots/matlab/ajouterArc.m
@@ -0,0 +1,3 @@
+ajouterArc := proc(G, s_deb, s_arr, value)
+
+end_proc:
--- proj-2-flots/matlab/ff_flots.m
+++ proj-2-flots/matlab/ff_flots.m
@@ -20,60 +20,48 @@ function F = ff_flots(G, s, t)
 % function: Short description
 F = G;
 % Extended description
-
-    continuer = 1;  %  == true
-    while continuer
-        flot_aux = [];
-        beta = Inf;
-        sommet_courant = s;
-        voisins_visite = [s];
-        while (sommet_courant ~= t)
-%             disp('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-%             disp(sommet_courant)
-            VD = voisins_dir(F,sommet_courant);
+continuer = 1;  %  == true
+while continuer
+    flot_aux = [];
+    beta = Inf;
+    sommet_courant = s;
+    voisins_visite = [s];
+    while (sommet_courant ~= t)
+        VD = voisins_dir(F,sommet_courant);
+        i = 1;
+        while ((i <= length(VD))  && (capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant)) == 0)
+            i = i + 1;
+        end
+        % not ((capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant)) == 0)  || not (i < length(VD))
+        if i <= length(VD)
+            flot_aux = [flot_aux, 1];
+            voisins_visite=[voisins_visite,VD(i)];   % on a le prochain voisin
+            beta = min(beta, capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant));
+            sommet_courant = VD(i);
+            c = capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant);
+        else
+            % on regarde les voisins indirect
+            VI = voisins_ind(F,sommet_courant);
             i = 1;
-            capacite(F,sommet_courant, VD(i))
-            flot(F,VD(i), sommet_courant)
-            while ((i <= length(VD))  && (capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant)) == 0) 
+            while (i <= length(VI) && flot(VI(i), sommet_courant) < 0 )
                 i = i + 1;
             end
-            % not ((capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant)) == 0)  || not (i < length(VD))
-            if i <= length(VD)  
-                flot_aux = [flot_aux, 1];
-                voisins_visite=[voisins_visite,VD(i)];   % on a le prochain voisin
-                beta = min(beta, capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant));
-                sommet_courant = VD(i);
-                c = capacite(F,sommet_courant, VD(i)) - flot(F,VD(i), sommet_courant)
-%                 disp('VD')
+            if i <= length(VI)
+                flot_aux = [flot_aux, -1];
+                voisins_visite=[voisins_visite,VI(i)];   % on a le prochain voisin
+                beta = min(beta, flot(F,VI(i), sommet_courant));
+                sommet_courant = VI(i);
             else
-                % on regarde les voisins indirect
-                VI = voisins_ind(F,sommet_courant);
-                i = 1;
-                disp('VI')
-                while (i <= length(VI) && flot(VI(i), sommet_courant) < 0 )  
-                    i = i + 1;
-                end
-                if i <= length(VI)
-                    flot_aux = [flot_aux, -1];
-                    voisins_visite=[voisins_visite,VI(i)];   % on a le prochain voisin
-                    beta = min(beta, flot(F,VI(i), sommet_courant));
-                    sommet_courant = VI(i);
-%                     disp('VI')
-                else
-                    % on ne peut pas continuer avec ce sommet
-                    % on doit retourner en arrière
-%                     disp('alo')
-                    break
-                end
+                % on ne peut pas continuer avec ce sommet
+                % on doit retourner en arrière
+                break
             end
-%             flot_aux
-            beta
-        end
-        F = maj_flot(F, voisins_visite, beta * flot_aux)
-        flot_aux
-        if length(flot_aux) == 0
-            continuer = 0;
         end
     end
-      % function
-end
+    F = maj_flot(F, voisins_visite, beta * flot_aux)
+    if length(flot_aux) == 0
+        continuer = 0;
+    end
+end % while principale
+
+end % function
